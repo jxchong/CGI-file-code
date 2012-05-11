05e88361952d6bb77cc596ff2e165880b2848b24
Jessica Chong <jxchong@gmail.com>
2012-05-11 16:13:02 -0500

    dbNSFP v2beta results are integrated
    however there are no results for indel variants or for variants in
    genes where Encode doesn't have an annotated transcript

--- calc_popinfo.pl
+++ calc_popinfo.pl
@@ -12,15 +12,17 @@ use Getopt::Long;
 use IO::Uncompress::Bunzip2 qw(bunzip2 $Bunzip2Error);
 
 
-my ($testvarfile, $outputfile, $geneannotationlistvar, %geneannotations, $dbnsfpfile);
-my @dbnsfp_keepcol = c(14, 17, 21..34, 37);
+my ($testvarfile, $outputfile, $geneannotationlistvar, %geneannotations, $dbnsfp_folder);
+my %dbnsfp_annots;
+my $dbnsfp_annot_ref = \%dbnsfp_annots;
+my @dbnsfp_keepcol = (14, 17, 18, 21..34, 37, 47, 48);
 
 
 GetOptions(
 	'testvar=s' => \$testvarfile, 
 	'o=s' => \$outputfile,
 	'genelistvar=s' => \$geneannotationlistvar,
-	'dbnsfp=s' => \$dbnsfpfile,
+	'dbnsfp=s' => \$dbnsfp_folder,
 );
 
 if (!defined $testvarfile) {
@@ -29,9 +31,20 @@ if (!defined $testvarfile) {
 	optionUsage("option --genelistvar not defined\n");
 }	elsif (!defined $outputfile) {
 	optionUsage("option --o not defined\n");
-} elsif (!defined $dbnsfpfile) {
-	optionUsage("option --dbnsfp not defined\n");
+} elsif (!defined $dbnsfp_folder) {
+	optionUsage("option --dbnsfp folder not defined\n");
 }
+
+if (! -e $testvarfile) { 
+	die "$testvarfile does not exist\n";
+}
+if (! -e $geneannotationlistvar) {
+	die "$geneannotationlistvar does not exist\n";
+}
+if (! -e $dbnsfp_folder) {
+	die "$dbnsfp_folder does not exist\n";
+}
+
 
 # load in gene annotations from files
 print STDERR "Preloading gene annotations from $geneannotationlistvar\n";
@@ -48,24 +61,28 @@ while (<GENELISTVAR>) {
 close GENELISTVAR;
 
 
-# prepare dbNSFP file for reading
-open( my $dbnsfp_handle, "zcat $dbnsfpfile |" ) or die "Couldn't read $dbnsfpfile: $!";
-my $dbnsfp_headerline = <$dbnsfp_handle>;
+# get header for dbNSFP annotations
+my $dbnsfp_headerline;
+my $dbnsfpfile = "$dbnsfp_folder/dbNSFP2.0b1_variant.chr1";
+if (-e "$dbnsfpfile.bz2") {
+	$dbnsfp_headerline = `bzcat $dbnsfpfile.bz2 | head -1`;
+} else {
+	$dbnsfp_headerline = `head -1 $dbnsfpfile.tsv`;
+}	
+$dbnsfp_headerline =~ s/\s+$//;
+$dbnsfp_headerline =~ s/^#//;
 my @dbnsfp_header = split("\t", $dbnsfp_headerline);
-my $dbnsfp_nextchrline;			# read through dbNSFP file until new chr, but that line needs to be saved
-my %dbnsfp_annots;
-my $dbnsfp_annot_ref = \%dbnsfp_annots;
+
 
 
 open (OUT, ">$outputfile");
 print OUT "chr\tbegin\tend\tvartype\tref\talt\txref\taltfreq\tMAF\tnhomref\tnhet\tnhomalt\tnmiss\tnhommaj\tnhommin\tCR\tgenesymbol\torientation\tcomponent\tcomponentIndex\thasCodingRegion\timpact\tnucleotidePos\tproteinPos\tannotationRefSequence\tsampleSequence\tgenomeRefSequence";
-print OUT join("\t", @dbnsfp_header[@dbnsfp_keepcol])."\n";
+print OUT "\t".join("\t", @dbnsfp_header[@dbnsfp_keepcol])."\n";
 if ($testvarfile =~ /.bz2$/) {
 	open (FILE, "bzcat $testvarfile |") or die "Cannot read $testvarfile: $!\n";
 } else {
 	open (FILE, "$testvarfile") or die "Cannot read $testvarfile: $!\n";
 }
-open (FILE, "$testvarfile") or die "Cannot read $testvarfile file.\n";
 my $header = <FILE>;
 $header =~ s/\s+$//;
 my @headerline = split("\t", $header);
@@ -77,20 +94,17 @@ while ( <FILE> ) {
 	my ($thischr, $thisstart, $thisend) = @line[1..3];
 	my $vartype = $line[4];
 	my ($ref, $alleleSeq) = @line[5..6];
-	
-	if ($thischr eq 'chr3') {
-		exit;
-	}
-	
+
+	# if ($thischr eq 'chr1' && $thisstart > 23974) {				 # DEBUG
+	# 	exit;
+	# }
+
 	if ($thischr ne $currchr) {
-		print STDERR "Working on $thischr\n";			
+		print STDERR "Reading dbNSFP for $thischr\n";			
 		$currchr = $thischr;
 		%dbnsfp_annots = ();							# clear out previous chromosome's annotations
-		if ($currchr ne 'NA') {
-			my @nextline = split("\t", $dbnsfp_nextchrline);
-			$dbnsfp_annots{$nextline[1]} = @nextline[@dbnsfp_keepcol];		# load the first line of the new chr
-		}
-		($dbnsfp_nextchrline, $dbnsfp_annot_ref) = loaddbNSFP($dbnsfp_annot_ref, $thischr, $dbnsfp_handle, \@dbnsfp_keepcol);
+		$dbnsfp_annot_ref = loaddbNSFP($dbnsfp_annot_ref, $thischr, \@dbnsfp_keepcol, $dbnsfp_folder);
+		print STDERR "Working on $thischr\n";		
 	}
 
 	my $thiscoord = join('_', @line[1..6]);
@@ -117,7 +131,7 @@ while ( <FILE> ) {
 	
 	my $ngenosubj = $genocounts[0]+$genocounts[1]+$genocounts[2];	
 	
-	if ($ngenosubj != 0) {	
+	if ($ngenosubj != 0) {			# only print out lines that have at least one (high quality) genotyped individual!!
 		my $altfreq = ($genocounts[1]+2*$genocounts[2])/(2*$ngenosubj);
 		my $callrate = 1-($genocounts[3]/scalar(@genotypes));
 	
@@ -132,56 +146,63 @@ while ( <FILE> ) {
 			$genocounts_relhutt_min = $genocounts[0];
 		}
 
-		my $geneannot = (("\t") x 11);
+		my $geneannot = (("\t") x 10);
 		if (exists $geneannotations{$thiscoord}) {
 			$geneannot = $geneannotations{$thiscoord};
 		} 
 		
+		# print output (combine genotypes with CGI gene annotations and dbNSFP annotations)
 		for (my $i=1; $i<=7; $i++) {
-			if ($line[$i]) {
+			if ($line[$i] ne "") {
 				print OUT "$line[$i]\t";
 			} else {
 				print OUT "\t";
 			}
 		}
 		print OUT "$altfreq\t$maf\t".join("\t", @genocounts)."\t$genocounts_relhutt_maj\t$genocounts_relhutt_min\t$callrate\t$geneannot";
-		
+			
 		my $dbnsfp_lookup = join("_", ($thischr, $thisend, $ref, $alleleSeq));
-		if (($thisend-$thisstart)==1 && exists $dbnsfp_annots{$thisend}) {		# haven't decided what to print out for ins/del/complex variants that span multiple bases
-			print OUT "$dbnsfp_annots{$thisend}\n";		
+		if (($thisend-$thisstart)==1 && exists $dbnsfp_annots{$dbnsfp_lookup}) {		# haven't decided what to print out for ins/del/complex variants that span multiple bases		
+			print OUT "\t$dbnsfp_annots{$dbnsfp_lookup}\n";		
 		} else {
-			print OUT (("\t") x scalar(@dbnsfp_keepcol))."\n";			
+			print OUT "\t".(("\t") x scalar(@dbnsfp_keepcol))."\n";			
 		}
+		# END print output (combine genotypes with CGI gene annotations and dbNSFP annotations)
 	}
 }
 close FILE;
 close OUT;
 
-close $dbnsfp_handle;
 
 
 sub loaddbNSFP {
 	my $dbnsfp_annot_ref = $_[0];
 	my $desiredchr = $_[1];
-	my $dbnsfp_handle = $_[2];
-	my $dbnsfp_keepcol_ref = $_[3];
-	my $dbnsfp_nextchrline;
+	my $dbnsfp_keepcol_ref = $_[2];
+	my $dbnsfp_folder = $_[3];
 	
-	while (<$dbnsfp_handle>) {
-		$dbnsfp_nextchrline = $_;
-		my @line = split("\t", $dbnsfp_nextchrline);
-		my ($thischr, $pos, $ref, $alt) = @line[0..3];
-		my $dbnsfp_lookup = join("_", ($thischr, $pos, $ref, $alt));
-		
-		if ($thischr ne $desiredchr) {
-			last;
+	# prepare dbNSFP file for reading
+	my $dbnsfp_handle;
+	my $dbnsfpfile = "$dbnsfp_folder/dbNSFP2.0b1_variant.$desiredchr";
+	if (-e "$dbnsfpfile.bz2" || -e $dbnsfpfile) {
+		if (-e "$dbnsfpfile.bz2") {
+			open ($dbnsfp_handle, "bzcat $dbnsfpfile.bz2 |") or die "Cannot read $dbnsfpfile: $!\n";
 		} else {
+			open ($dbnsfp_handle, "$dbnsfpfile") or die "Cannot read $dbnsfpfile: $!\n";
+		}
+		<$dbnsfp_handle>;				# skip header
+		while (<$dbnsfp_handle>) {
+			$_ =~ s/\s+$//;
+			my @line = split("\t", $_);
+			my ($thischr, $pos, $ref, $alt) = @line[0..3];
+			my $dbnsfp_lookup = join("_", ("chr$thischr", $pos, $ref, $alt));
 			# parse and save desired fields
 			${$dbnsfp_annot_ref}{$dbnsfp_lookup} = join("\t", @line[@{$dbnsfp_keepcol_ref}]);
 		}
+		close $dbnsfp_handle;
 	}
 	
-	return ($dbnsfp_nextchrline, $dbnsfp_annot_ref);
+	return $dbnsfp_annot_ref;
 }
 
 
@@ -193,7 +214,11 @@ sub loadGeneAnnot {
 	my @line = split ("\t", $_);
 	my ($thischr, $thisstart, $thisend) = @line[0..2];
 	
-	my $lookup = my $thiscoord = join('_', @line[0..5]);
+	# if ($thischr eq 'chr1' && $thisstart > 23974) {				 # DEBUG
+	# 	last;
+	# }
+	
+	my $lookup = join('_', @line[0..5]);
 	
 	# 0 chromosome	begin	end	varType	reference	call	xRef
 	# 7 geneId	mrnaAcc	proteinAcc	symbol	orientation	component	componentIndex
@@ -222,7 +247,7 @@ sub optionUsage {
 	print "perl $0 \n";
 	print "\t--testvar\ttestvariants file\n";
 	print "\t--genelistvar\tgenelistvar file (produced by Generate_Gene_ListVariants_From_Gene_File_0_1_4.pl)\n";
-	print "\t--dbnsfp\tdbnsfp.zip file (from http://sites.google.com/site/jpopgen/dbNSFP)\n";
+	print "\t--dbnsfp\tfolder containing dbnsfp files, one per chr (from http://sites.google.com/site/jpopgen/dbNSFP)\n";
 	print "\t--o\toutput <output.popinfo.tsv> file\n";
 	die;
 }
